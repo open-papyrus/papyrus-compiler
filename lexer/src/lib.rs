69395c803b4c5b91aca518925b3000dc27850686
erri120 <erri120@protonmail.com>
2022-06-02 20:31:24 +0200

    Create wrapper for better access

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,1 +1,23 @@
-mod syntax;
+pub mod syntax;
+
+pub struct Lexer<'a> {
+    lex: logos::Lexer<'a, syntax::token::Token<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        use logos::Logos;
+
+        Self {
+            lex: syntax::token::Token::lexer(input),
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = syntax::token::Token<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.lex.next()
+    }
+}
