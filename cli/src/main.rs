2990adddc520e09dc1aaa44181c5e6e186edbe4f
erri120 <erri120@protonmail.com>
2022-06-02 20:31:34 +0200

    Update CLI to print Lexer result

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,40 @@
+use anyhow::{anyhow, Context};
+use clap::Parser;
+use papyrus_lexer::Lexer;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(parse(from_os_str))]
+    input_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+    match run(&args) {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("{}", err);
+        }
+    }
+}
+
+fn run(args: &Args) -> Result<(), anyhow::Error> {
+    if !args.input_path.exists() {
+        return Err(anyhow!(
+            "Input path {} does not exist!",
+            args.input_path.display()
+        ));
+    }
+
+    let script = fs::read_to_string(args.input_path.clone())
+        .with_context(|| format!("Unable to read input file {}", args.input_path.display()))?;
+
+    let lexer = Lexer::new(script.as_str());
+    for token in lexer {
+        println!("{:?}", token);
+    }
+
+    Ok(())
 }
