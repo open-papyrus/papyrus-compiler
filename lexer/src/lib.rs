8e70718c9f533b4e907d24afa7561a3a9cce5df6
erri120 <erri120@protonmail.com>
2022-06-05 15:33:17 +0200

    Simplify lexer

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,61 +1,11 @@
 use crate::syntax::token::Token;
+use logos::{Logos, Span};
 
 pub mod syntax;
 
-pub struct Lexer<'a> {
-    lex: logos::Lexer<'a, Token<'a>>,
-}
-
-impl<'a> Lexer<'a> {
-    pub fn new(input: &'a str) -> Self {
-        use logos::Logos;
-
-        Self {
-            lex: Token::lexer(input),
-        }
-    }
-
-    pub fn lex_all(input: &'a str) -> Vec<Token<'a>> {
-        let me = Self::new(input);
-        let tokens: Vec<Token<'a>> = me.collect();
-        tokens
-    }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = Token<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.lex.next()
-    }
-}
-
-pub struct SpannedLexer<'a> {
-    iter: logos::SpannedIter<'a, Token<'a>>,
-}
-
-impl<'a> SpannedLexer<'a> {
-    pub fn new(input: &'a str) -> Self {
-        use logos::Logos;
-
-        Self {
-            iter: Token::lexer(input).spanned(),
-        }
-    }
-
-    pub fn lex_all(input: &'a str) -> Vec<SpannedToken<'a>> {
-        let me = Self::new(input);
-        let tokens: Vec<SpannedToken<'a>> = me.collect();
-        tokens
-    }
-}
-
-pub type SpannedToken<'a> = (Token<'a>, core::ops::Range<usize>);
-
-impl<'a> Iterator for SpannedLexer<'a> {
-    type Item = SpannedToken<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next()
-    }
+pub fn run_lexer<'a>(input: &'a str) -> Vec<(Token<'a>, Span)> {
+    let lexer: logos::Lexer<'a, Token<'a>> = Token::lexer(input);
+    let iter: logos::SpannedIter<'a, Token<'a>> = lexer.spanned();
+    let tokens: Vec<_> = iter.collect();
+    tokens
 }
