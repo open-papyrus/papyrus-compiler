f0f83cf4dfcacc00a481689aa0073883c32718e5
erri120 <erri120@protonmail.com>
2022-06-16 13:11:48 +0200

    Allow certain flags as identifiers

--- parser/src/ast/identifier.rs
+++ parser/src/ast/identifier.rs
@@ -1,11 +1,15 @@
 use crate::parse::TokenParser;
 use chumsky::prelude::*;
+use papyrus_compiler_lexer::syntax::keyword_kind::KeywordKind;
 use papyrus_compiler_lexer::syntax::token::Token;
 
 pub type Identifier<'a> = &'a str;
 
 pub fn identifier_parser<'a>() -> impl TokenParser<'a, Identifier<'a>> {
     select! {
-        Token::Identifier(value) => value
+        Token::Identifier(value) => value,
+
+        // some keywords, mostly flags, are allowed to be parsed as an identifier
+        Token::Keyword(KeywordKind::Default) => "default"
     }
 }
