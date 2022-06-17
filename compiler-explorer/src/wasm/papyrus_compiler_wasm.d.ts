1479c75f10dbd059ed3285ffd38f92d7b728ac5e
erri120 <erri120@protonmail.com>
2022-06-17 17:12:04 +0200

    Add WASM output

---
+++ compiler-explorer/src/wasm/papyrus_compiler_wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} src
+* @returns {string}
+*/
+export function run_parser(src: string): string;
---
+++ wasm/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn run_parser(src: &str) -> Result<String, JsValue> {
+    let res = papyrus_compiler_core::compile_string(0, src);
+    match res {
+        Ok(script ) => Ok(format!("{:?}", script)),
+        Err(_reports) => Err(JsValue::null())
+    }
+}
