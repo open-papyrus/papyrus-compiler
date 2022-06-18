d6fecf31eedc021f5878b26f0a337dfec36f7777
erri120 <erri120@protonmail.com>
2022-06-18 17:44:27 +0200

    Update WASM

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,10 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn run_parser(src: &str) -> Result<String, JsValue> {
+pub fn run_parser(src: &str) -> String {
     let res = papyrus_compiler_core::compile_string(0, src);
     match res {
-        Ok(script) => Ok(format!("{:#?}", script)),
-        Err(_reports) => Err(JsValue::from("Error compiling the script!")),
+        Ok(script) => format!("{:#?}", script),
+        Err(diagnostics) => {
+            let mut res = String::with_capacity(diagnostics.len() * 50);
+
+            for diagnostic in diagnostics {
+                let to_append = format!(
+                    "[{}{:03}] {:?} {}",
+                    diagnostic.prefix(),
+                    diagnostic.id(),
+                    diagnostic.range(),
+                    diagnostic.message()
+                );
+                res.push_str(to_append.as_str());
+            }
+
+            res
+        }
     }
 }
