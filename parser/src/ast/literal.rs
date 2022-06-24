f2d5fbbcab4d48162de42e210732d14c0638614b
erri120 <erri120@protonmail.com>
2022-06-24 14:45:11 +0200

    Implement Literal Parser

--- parser/src/ast/literal.rs
+++ parser/src/ast/literal.rs
@@ -1,28 +1,30 @@
-use crate::parse::TokenParser;
-use chumsky::prelude::*;
+use crate::parser::{Parse, Parser, ParserResult};
+use crate::select_tokens;
 use papyrus_compiler_lexer::syntax::token::Token;
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Literal<'a> {
+pub enum Literal<'source> {
     Boolean(bool),
     Integer(i32),
     Float(f32),
-    String(&'a str),
+    String(&'source str),
     None,
 }
 
-pub fn literal_parser<'a>() -> impl TokenParser<'a, Literal<'a>> {
-    select! {
-        Token::BooleanLiteral(value) => Literal::Boolean(value),
-        Token::IntegerLiteral(value) => Literal::Integer(value),
-        Token::FloatLiteral(value) => Literal::Float(value),
-        Token::StringLiteral(value) => Literal::String(value),
-        Token::NoneLiteral => Literal::None,
+impl<'source> Parse<'source> for Literal<'source> {
+    fn parse(parser: &mut Parser<'source>) -> ParserResult<'source, Self> {
+        select_tokens!(parser, "Literal",
+            Token::BooleanLiteral(value) => Literal::Boolean(*value),
+            Token::IntegerLiteral(value) => Literal::Integer(*value),
+            Token::FloatLiteral(value) => Literal::Float(*value),
+            Token::StringLiteral(value) => Literal::String(*value),
+            Token::NoneLiteral => Literal::None
+        )
     }
 }
 
-impl<'a> Display for Literal<'a> {
+impl<'source> Display for Literal<'source> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Literal::Boolean(value) => write!(f, "{}", value),
