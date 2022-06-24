ae163c252fbd3e791ca2c7b9e56a5e7e43f2374f
erri120 <erri120@protonmail.com>
2022-06-24 13:27:13 +0200

    Implement Identifier Parser

--- parser/src/ast/identifier.rs
+++ parser/src/ast/identifier.rs
@@ -1,30 +1,13 @@
-use crate::parse::TokenParser;
-use crate::parser::{CustomParser, ParserError, ParserResult};
+use crate::parser::{Parse, Parser, ParserResult};
 use crate::select_tokens;
-use chumsky::prelude::*;
-use papyrus_compiler_lexer::syntax::keyword_kind::KeywordKind;
 use papyrus_compiler_lexer::syntax::token::Token;
 
-pub type Identifier<'a> = &'a str;
+pub type Identifier<'source> = &'source str;
 
-pub fn identifier_parser<'a>() -> impl TokenParser<'a, Identifier<'a>> {
-    select! {
-        Token::Identifier(value) => value,
-
-        // some keywords, mostly flags, are allowed to be parsed as an identifier
-        Token::Keyword(KeywordKind::Default) => "default"
+impl<'source> Parse<'source> for Identifier<'source> {
+    fn parse(parser: &mut Parser<'source>) -> ParserResult<'source, Self> {
+        select_tokens!(parser, "Identifier",
+            Token::Identifier(value) => *value
+        )
     }
-}
-
-pub(crate) fn custom_identifier_parser<'source>(
-    parser: &mut CustomParser<'source>,
-) -> ParserResult<'source, Identifier<'source>> {
-    select_tokens!(parser,
-        Token::Identifier(value) => value.clone(),
-        
-        // some keywords, mostly flags, are allowed to be parsed as an identifier
-        Token::Keyword(KeywordKind::Default) => "default";
-        
-        vec![Token::Identifier(""), Token::Keyword(KeywordKind::Default)]
-    )
 }
