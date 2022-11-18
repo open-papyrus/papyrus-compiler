96e62333e88cee64cfee390b66f98ee56cdf9d21
erri120 <erri120@protonmail.com>
2022-11-18 16:30:31 +0100

    Transfer to open-papyrus/compiler-explorer

--- compiler-explorer/src/index.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
-
-import App from './components/App';
-import './index.css';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
